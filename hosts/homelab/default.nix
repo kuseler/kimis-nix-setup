a5c2538513448124d9daadd6307557c274d14eb3
Kimi Müller <kimi-mueller@protonmail.com>
2026-01-17 17:09:36 +0100

    add k3s for cluster

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -5,5 +5,18 @@
     ../../nixos
   ];
 
+  services.k3s = {
+    enable = true;
+    role = "server";  # Use "agent" if this is a worker node joininig a cluster
+    # Makes the kubeconfig readable so you don't always need sudo for kubectl
+    extraFlags = toString [
+      "--write-kubeconfig-mode 644"
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 6443 ];
+  networking.firewall.allowedUDPPorts = [ 8472 ];
   networking.hostName = "homelab";
+
+  environment.systemPackages = [ pkgs.k3s ];
 }
